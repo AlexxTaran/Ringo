b1523eea3d71e495b5cecc8350f81c7cc6a55880
AlexxTaran <104693870+AlexxTaran@users.noreply.github.com>
2022-07-05 16:09:54 +0300

    Add files via upload

--- Ringo/js/main.js
+++ Ringo/js/main.js
@@ -22,19 +22,45 @@ const swiper = new Swiper('.intro__swiper', {
 
   const swiper2 = new Swiper('.work__swiper', {
     loop: true,
-    slidesPerView: "auto",
-    spaceBetween: 40,
+    slidesPerView: 1,
+    spaceBetween: 100,
 
     navigation: {
       nextEl: '.work__button-next',
       prevEl: '.work__button-prev',
     },
+
+    pagination: {
+      el: '.work__pagination',
+      type: 'bullets',
+      bulletClass: 'work__pagination_bullet',
+      bulletActiveClass: 'work__pagination_bullet-active',
+      clickable: true,
+    },
+    breakpoints: {
+      325: {
+        slidesPerView: "auto",
+      },
+      650: {
+        slidesPerView: "auto",
+        spaceBetween: 40,
+      }
+    },
     centeredSlides: true,
   });
 
 
-
-  //accept
-  document.querySelector('.contacts__checkbox-wrap').addEventListener('click', () => {
-    document.querySelector('.accept>span').classList.toggle('accept-active');
-  });
+//burger
+document.querySelector('.burger').addEventListener('click', () => {
+  document.querySelector('.header__nav').classList.toggle('header__nav-active');
+  document.querySelector('.burger').classList.toggle('burger-active');
+  if (document.querySelector('.burger').classList.contains('burger-active')){
+    document.body.style.overflow = "hidden";
+  }else{
+    document.body.style.overflow = "unset";
+  };
+});
+//accept
+document.querySelector('.contacts__checkbox-wrap').addEventListener('click', () => {
+  document.querySelector('.accept>span').classList.toggle('accept-active');
+});
