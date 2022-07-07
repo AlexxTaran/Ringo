c4a4a1aad67c234135aa6cf750a61faf259965f6
AlexxTaran <104693870+AlexxTaran@users.noreply.github.com>
2022-07-07 19:15:28 +0300

    Add files via upload

--- Ringo/js/main.js
+++ Ringo/js/main.js
@@ -50,7 +50,8 @@ const swiper = new Swiper('.intro__swiper', {
   });
 
 
-//burger
+//------------burger--------------
+
 document.querySelector('.burger').addEventListener('click', () => {
   document.querySelector('.header__nav').classList.toggle('header__nav-active');
   document.querySelector('.burger').classList.toggle('burger-active');
@@ -60,7 +61,64 @@ document.querySelector('.burger').addEventListener('click', () => {
     document.body.style.overflow = "unset";
   };
 });
-//accept
+
+//-------------accept----------------
+
 document.querySelector('.contacts__checkbox-wrap').addEventListener('click', () => {
   document.querySelector('.accept>span').classList.toggle('accept-active');
+});
+
+//----------------scroll image-------------
+
+const cards = document.querySelectorAll('.js-scroll_container');
+
+const callback = (elements) => {
+    elements.forEach(ele => {
+        if(ele.isIntersecting  && !ele.target.classList.contains('js-scroll_active')){
+            ele.target.classList.add('js-scroll_active')
+        }//else{
+        //     ele.target.classList.remove('js-scroll_active')
+        // }
+    })
+}
+
+const observer = new IntersectionObserver(callback);
+cards.forEach(card => observer.observe(card));
+
+
+//--------------scroll text-------------
+
+const title = document.querySelectorAll('h1 , .intro__content>p , h2');
+
+const callbacks = (element) => {
+    element.forEach(elem => {
+        if(elem.isIntersecting  && !elem.target.classList.contains('title_active')){
+            elem.target.classList.add('title_active')
+        }//else{
+        //     elem.target.classList.remove('title_active')
+        // }
+    })
+}
+
+const observers = new IntersectionObserver(callbacks);
+title.forEach(titleCard => observers.observe(titleCard));
+
+//---------active cursor------------
+
+document.querySelectorAll('.benefits__image-wrap img').forEach(pic => {
+  const fill = e => {
+  pic.style.left = e.offsetX + "px";
+};
+  pic.addEventListener('mousemove', fill);
+});
+
+//----------form---------------
+
+document.querySelectorAll('.site-button_form').forEach(fEl => {
+  fEl.addEventListener('click', () =>{
+    document.querySelector('.form_pop').classList.add('form_pop-active');
+  });
+});
+document.querySelector('.form__closed').addEventListener('click', () => {
+  document.querySelector('.form_pop').classList.remove('form_pop-active');
 });
